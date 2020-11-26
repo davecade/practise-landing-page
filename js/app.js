44e4f1bbbbc4751508ff7a43fb278a7c68568779
Dave <dave.cadelina@outlook.com>
2020-11-27 00:10:58 +1100

    add modal functionality with fade in and out

--- js/app.js
+++ js/app.js
@@ -4,10 +4,22 @@ const App = (() => {
     const hamburgerEl = document.querySelector('.hamburger')
     const sliderEl = document.querySelector('#slider')
     const mainPageEl = document.querySelector('#main-page')
+    const signUpEl = document.querySelector('.sign-up')
+    const closeEl = document.querySelector('.close')
+    const modalEl = document.querySelector('.modal')
+    const modalContentEl = document.querySelector('.modal-content')
 
     // -- Eventlisteners
     const Eventlisteners = () => {
         hamburgerEl.addEventListener('click', toggleSlider)
+        closeEl.addEventListener('click', toggleModal)
+        signUpEl.addEventListener('click', toggleModal)
+
+        modalEl.addEventListener('click', (event) => {
+            if(event.target.matches(".modal")) {
+                toggleModal();
+            }
+        })
     }
 
     // -- Methods
@@ -21,11 +33,29 @@ const App = (() => {
         }
     }
 
+    const toggleModal =() => {
+        console.log(modalEl.style.visibility)
+
+        if(modalEl.style.visibility == 'hidden' && modalContentEl.style.visibility == 'hidden') {
+            modalEl.style.visibility = "visible";
+            modalEl.style.opacity = "1"
+            modalContentEl.style.visibility = "visible";
+            modalContentEl.style.opacity = "1";
+        } else {
+            modalEl.style.visibility = 'hidden';
+            modalEl.style.opacity = "0"
+            modalContentEl.style.visibility = 'hidden';
+            modalContentEl.style.opacity = "0";
+        }
+    }
+
     const init = () => {
         render()
         Eventlisteners();
         sliderEl.style.left = "-200px";
         mainPageEl.style.left = "0px"
+        modalEl.style.visibility = 'hidden';
+        modalContentEl.style.visibility = 'hidden';
     }
 
     const render = () => {
